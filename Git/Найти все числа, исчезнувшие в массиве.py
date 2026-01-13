ecade3fc45f845af60e1f85b15ed8801892241b8
Alxan2007 <alxanmac@gmail.com>
2026-01-13 12:34:48 +0300

    Update Найти все числа, исчезнувшие в массиве.py

--- Git/Найти все числа, исчезнувшие в массиве.py
+++ Git/Найти все числа, исчезнувшие в массиве.py
@@ -1,8 +1,1 @@
-class Solution(object):
-    def findDisappearedNumbers(self, nums):
-        num = set(nums)
-        x = []
-        for i in range(1, len(nums) + 1):
-            if i not in num:
-                x.append(i)
-        return x
+
