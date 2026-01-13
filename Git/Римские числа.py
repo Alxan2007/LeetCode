ab92d54717f248ecdf0807b94eac9f94269ef70d
Alxan2007 <alxanmac@gmail.com>
2026-01-13 12:34:25 +0300

    Update Римские числа.py

--- Git/Римские числа.py
+++ Git/Римские числа.py
@@ -1,21 +1,2 @@
-class Solution(object):
-    def romanToInt(self, s):
-        roman_dict = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000
-        }
-        result = 0
-        for i in range(len(s)):
-            current = roman_dict[s[i]]
-            if i + 1 < len(s) and roman_dict[s[i + 1]] > current:
-                result -= current
-            else:
-                result += current
-        return result
 
 
